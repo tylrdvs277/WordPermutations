1d911d93ced2936c8f9c1765e3c3b9f0c0477301
Tyler <tylrdvs277@gmail.com>
2017-09-28 17:53:24 -0700

    28 September 201728 September 2017

---
+++ perm_lex_testcases.py
@@ -0,0 +1,20 @@
+import unittest
+import perm_lex
+
+class TestAssign1(unittest.TestCase):
+
+    def test_perm_gen_lex_1(self):
+        self.assertEqual(perm_lex.perm_gen_lex("ab"),["ab","ba"])
+
+    def test_perm_gen_lex_2(self):
+    	self.assertEqual(perm_lex.perm_gen_lex("a"),["a"])
+
+    def test_perm_gen_lex_3(self):
+    	self.assertEqual(perm_lex.perm_gen_lex(""),[])
+
+    def test_perm_gen_lex_4(self):
+    	result = ["abc", "acb", "bac", "bca", "cab", "cba"]
+    	self.assertEqual(perm_lex.perm_gen_lex("abc"),result)
+
+if __name__ == "__main__":
+        unittest.main()
